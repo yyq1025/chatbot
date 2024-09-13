e15a8d980b79319f9069641406ac6a7bf3c46ec5
Yueqian Yang <yueqian8@illinois.edu>
2024-09-13 09:55:01 -0500

    change url

--- components/header.tsx
+++ components/header.tsx
@@ -57,7 +57,7 @@ export function Header() {
       <div className="flex items-center justify-end space-x-2">
         <a
           target="_blank"
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
+          href="https://github.com/yyq1025/chatbot"
           rel="noopener noreferrer"
           className={cn(buttonVariants({ variant: 'outline' }))}
         >
